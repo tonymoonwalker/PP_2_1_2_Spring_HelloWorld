44fd593f6ecf4c8f81c230114a5a3460c8803360
tonymoonwalker <tonymoonwalker@users.noreply.github.com>
2023-09-18 23:16:22 +0300

    2.1.3 done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        Cat catBean = applicationContext.getBean(Cat.class);
+        Cat catBean1 = applicationContext.getBean(Cat.class);
+        System.out.println(bean == bean1);
+        System.out.println(catBean == catBean1);
     }
 }
